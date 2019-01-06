f51495a22d1935ae5b19bbb25214a0c06d030fa3
Alexander Mohn <pogrmman@gmail.com>
2019-01-06 14:39:04 -0600

    Switched out consecutive lets for let*

--- test.scm
+++ test.scm
@@ -1,20 +1,18 @@
-;; Example usage of wiringPi bindings
-;; This program blinks an LED connected to a GPIO pin 
 (import (scheme base)
-	(scheme process-context)
-	(raspberry-pi gpio setup)
-	(raspberry-pi gpio pins)
-	(raspberry-pi gpio timing))
+        (scheme process-context)
+        (raspberry-pi gpio setup)
+        (raspberry-pi gpio pins)
+        (raspberry-pi gpio timing))
 
 (begin
-  (gpio-setup!)
-  (let ((args (command-line-arguments)))
-    (let ((pin (pin-mode! (string->number (car args)) OUTPUT)))
-      (let kernel ((counter 10))
-	(if (not (zero? counter))
-	    (begin
-	      (set-pin! pin HIGH)
-	      (wait 100)
-	      (set-pin! pin LOW)
-	      (wait 100)
-	      (kernel (- counter 1))))))))
+ (gpio-setup!)
+ (let* ((args (command-line-arguments))
+        (pin (pin-mode! (string->number (car args)) OUTPUT)))
+   (let kernel ((counter 10))
+    (if (not (zero? counter))
+        (begin
+         (set-pin! pin HIGH)
+         (wait 100)
+         (set-pin! pin LOW)
+         (wait 100)
+         (kernel (- counter 1)))))))
